b76c350ada9479e3143e323770f5d848184c8f21
Tornike Kharaishvili <tor.kharaishvili@gmail.com>
2017-03-11 02:46:07 +0400

    Request related extension methods

---
+++ src/MvcContortions/Misc/RequestExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Routing;
+
+namespace MvcContortions
+{
+    public static class RequestExtensions
+    {
+        public static string GetController(this HttpRequestBase request) => request.RequestContext.GetController();
+        public static string GetAction(this HttpRequestBase request) => request.RequestContext.GetAction();
+        public static string GetArea(this HttpRequestBase request) => request.RequestContext.GetArea();
+
+        public static string GetController(this HttpRequest request) => request.RequestContext.GetController();
+        public static string GetAction(this HttpRequest request) => request.RequestContext.GetAction();
+        public static string GetArea(this HttpRequest request) => request.RequestContext.GetArea();
+
+        private static string GetController(this RequestContext context) => context.GetValue("controller");
+        private static string GetAction(this RequestContext context) => context.GetValue("action");
+        private static string GetArea(this RequestContext context) => context.GetValue("area");
+        private static string GetValue(this RequestContext context, string key) => context.RouteData.Values[key]?.ToString();
+    }
+}
