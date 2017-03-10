10a6cf01df5204d31e87d798b901294a9daef51b
Tornike Kharaishvili <tor.kharaishvili@gmail.com>
2017-03-11 02:56:39 +0400

    SelectList related extension methods

--- src/MvcContortions/Misc/ExtensionMethods.cs
+++ src/MvcContortions/Misc/ExtensionMethods.cs
@@ -1,9 +1,14 @@
-﻿using System.Web;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace MvcContortions
 {
-    public static class RequestExtensions
+    public static class ExtensionMethods
     {
         public static string GetController(this HttpRequestBase request) => request.RequestContext.GetController();
         public static string GetAction(this HttpRequestBase request) => request.RequestContext.GetAction();
@@ -17,5 +22,11 @@ namespace MvcContortions
         private static string GetAction(this RequestContext context) => context.GetValue("action");
         private static string GetArea(this RequestContext context) => context.GetValue("area");
         private static string GetValue(this RequestContext context, string key) => context.RouteData.Values[key]?.ToString();
+
+
+        public static SelectList ToSelectList(this IEnumerable items) => new SelectList(items);
+        
+        public static SelectList ToSelectList<TModel, TValue, TText>(this IEnumerable<TModel> items, Expression<Func<TModel, TValue>> valueExpr, Expression<Func<TModel, TText>> textExpr) =>
+            new SelectList(items, ExpressionHelper.GetExpressionText(valueExpr), ExpressionHelper.GetExpressionText(textExpr));
     }
 }
